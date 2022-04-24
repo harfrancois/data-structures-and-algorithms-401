2324deeda3313f56222f1f09d94d53dafe8abaa2
Harvey Francois <har.francois@gmail.com>
2022-04-23 18:33:36 -0700

    not done yet

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -5,15 +5,35 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        self.end = "NULL"
 
     def insert(self, value):
         self.head = Node(value, self.head)
 
-#
+    def __str__(self):
+
+        return f"{self.end}"
+
+
+
+    def includes(self, target_value):
+        current = self.head
+        while current:
+            if current.value == target_value:
+                return True
+            current = current.next
+        return False
+
 class Node:
-    def __init__(self, value, next=None):
+    def __init__(self, value, next=None, end="NULL"):
         self.value = value
         self.next = next
+        self.end = end
+
+    # def __str__(self):
+    #
+    #     return f"{self.next} -> {self.end}"
+
 
 
 class TargetError:
