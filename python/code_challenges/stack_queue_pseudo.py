50a074165fb0ff1db027b249476e7d8bfcbf56ba
Harvey Francois <har.francois@gmail.com>
2022-05-02 21:47:47 -0700

    2 passing

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,21 @@
-from data_structures.stack import Stack
+from data_structures.linked_list import Node
 
 
 class PseudoQueue:
     pass
+
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, value):
+        self.rear = Node(value, self.rear)
+        if not self.front:
+            self.front = self.rear
+
+    def dequeue(self):
+        old_front = self.front
+        self.front = old_front.next
+        old_front.next = None
+
+        return old_front.value
