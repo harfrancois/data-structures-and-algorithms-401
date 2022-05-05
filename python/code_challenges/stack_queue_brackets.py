c19216cd79f44101321d01e793f67b26380ef063
Harvey Francois <har.francois@gmail.com>
2022-05-04 22:27:19 -0700

    all pass

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,36 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
+# validate function to check if the open string
+# has a closing to pare with it
+def validate(string_1, string_2):
+    if string_1 == '(' and string_2 == ')':
+        return True
+    elif string_1 == '{' and string_2 == '}':
+        return True
+    elif string_1 == '[' and string_2 == ']':
+        return True
+    else:
+        return False
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(input_string):
+    stack = Stack()
+    validation = True
+    string_index = 0
+    while string_index < len(input_string) and validation:
+        bracket = input_string[string_index]
+        if bracket in '({[':
+            stack.push(bracket)
+        else:
+            if stack.is_empty():
+                validation = False
+            else:
+                top = stack.pop()
+                if not validate(top, bracket):
+                    validation = False
+        string_index += 1
+
+    if stack.is_empty() and validation:
+        return True
+    else:
+        return False
+
