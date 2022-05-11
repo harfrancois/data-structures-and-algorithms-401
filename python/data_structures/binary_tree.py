3ade328bf35eb73d67423641e128915a40cdbc0a
Harvey Francois <har.francois@gmail.com>
2022-05-10 22:19:45 -0700

    not finished yet

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,6 +1,3 @@
-
-
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -13,15 +10,102 @@ class BinaryTree:
     def __init__(self):
         self.root = None
 
-    def pre_order(self, root):
-        stack = []
-        result = []
-        while root or stack:
-            while root:
-                result.append(self.root.value)
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            root = self.root.right
-        return result
+    def pre_order(self):
+        """
+        Traverse the tree in a pre-order fashion
+        return list of the values in correct order
+        """
 
+        def walk(root, values):  # root = a Node or none. Recursive function
+
+            # every recursive function needs to know when to stop, aka base case
+            if not root:
+                return
+
+            # Task 1: do something
+            values.append(root.value)
+
+            # Task 2: go left
+            walk(root.left, values)
+
+            # Task 3: go right
+            walk(root.right, values)
+
+        ordered_values = []
+
+        walk(self.root, ordered_values)
+
+        return ordered_values
+
+    def in_order(self):
+        """
+        Traverse the tree in an in-order fashion
+        return list of the values in correct order
+        """
+
+        def walk(root, values):  # root = a Node or none. Recursive function
+
+            # every recursive function needs to know when to stop, aka base case
+            if not root:
+                return
+
+            # Task 1: go left
+            walk(root.left, values)
+
+            # Task 2: do something
+            values.append(root.value)
+
+            # Task 3: go right
+            walk(root.right, values)
+
+        ordered_values = []
+
+        walk(self.root, ordered_values)
+
+        return ordered_values
+
+    def post_order(self):
+        """
+        Traverse the tree in a post-order fashion
+        return list of the values in correct order
+        """
+
+        def walk(root, values):  # root = a Node or none. Recursive function
+
+            # every recursive function needs to know when to stop, aka base case
+            if not root:
+                return
+
+            # Task 1: go left
+            walk(root.left, values)
+
+            # Task 2: go right
+            walk(root.right, values)
+
+            # Task 3: do something
+            values.append(root.value)
+
+        ordered_values = []
+
+        walk(self.root, ordered_values)
+
+        return ordered_values
+
+
+    def find_maximum_value(self):
+
+        def climb(root, values):
+
+            if not root:
+                return
+
+            if root.value > self.max:
+                self.max = root.value
+
+            climb(root.left, values)
+
+            climb(root.right, values)
+
+        climb(self.root, self.ordered_values)
+
+        return self.max
--- python/tests/data_structures/test_binary_tree.py
+++ python/tests/data_structures/test_binary_tree.py
@@ -2,30 +2,47 @@ import pytest
 from data_structures.binary_tree import BinaryTree, Node
 
 
-def test_exists():
+
+def test_node_exists():
+    assert Node
+
+
+
+def test_create_node():
+    node = Node("apple")
+    assert node
+    assert node.left is None
+    assert node.right is None
+
+
+
+def test_tree_exists():
     assert BinaryTree
 
 
 
+def test_tree_root_exists():
+    tree = BinaryTree()
+    assert tree.root is None
+
+
+
 def test_pre_order(tree):
-    actual = []
-    tree.pre_order(actual.append)
+    actual = tree.pre_order()
     expected = ["a", "b", "d", "e", "c", "f", "g"]
     assert actual == expected
 
 
 
 def test_in_order(tree):
-    actual = []
-    tree.in_order(actual.append)
-    expected = ["d", "b", "e", "a", "f", "c", "g"]
+    actual = tree.in_order()
+    expected = ["d", "b", "e", "a", "f", "c", "g"]  # left, root, right
     assert actual == expected
 
 
 
 def test_post_order(tree):
-    actual = []
-    tree.post_order(actual.append)
+    actual = tree.post_order()
     expected = ["d", "e", "b", "f", "g", "c", "a"]
     assert actual == expected
 
@@ -36,6 +53,8 @@ def tree():
           a
       b      c
     d  e    f  g
+    ["a", "b", "d", "e", "c", "f", "g"] - pre order
+    ["d", "b", "e", "a", "f", "c", "g"] # in order - left, root, right
     """
 
     tree = BinaryTree()
