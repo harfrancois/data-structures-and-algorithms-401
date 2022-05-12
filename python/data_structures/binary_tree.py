fd9bcfc65cdb5b1c8b9116f925bd29bb16f42eec
Harvey Francois <har.francois@gmail.com>
2022-05-11 23:14:12 -0700

    not finished

--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -7,8 +7,11 @@ class Node:
 
 class BinaryTree:
 
-    def __init__(self):
-        self.root = None
+    def __init__(self, root=None, values=None):
+        self.root = root
+        if values:
+            for value in values:
+                self.add(value)
 
     def pre_order(self):
         """
@@ -109,3 +112,29 @@ class BinaryTree:
         climb(self.root, self.ordered_values)
 
         return self.max
+
+ def add(self, value):
+
+        node = Node(value)
+
+        if not self.root:
+            self.root = node
+            return
+
+        breadth = Queue()
+
+        breadth.enqueue(self.root)
+
+        while not breadth.is_empty():
+            front = breadth.dequeue()
+            if not front.left:
+                front.left = node
+                return
+            else:
+                breadth.enqueue(front.left)
+
+            if not front.right:
+                front.right = node
+                return
+            else:
+                breadth.enqueue(front.right)
